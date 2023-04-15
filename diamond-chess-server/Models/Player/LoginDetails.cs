94b4035871f85cd0383091758423f2b4037bb435
vniej002 <juanr@bbd.co.za>
2023-04-15 14:57:09 +0200

    Added initial data layer.

---
+++ diamond-chess-server/Models/Player/LoginDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace diamond_chess_server.Models
+{
+    [Serializable]
+    public sealed class LoginDetails
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        //Not sure if we hash before sending to database.
+        public byte[] PasswordHash { get; set; }
+    }
+}
--- diamond-chess-server/Models/Player/Player.cs
+++ diamond-chess-server/Models/Player/Player.cs
@@ -1,11 +1,12 @@
 ﻿namespace diamond_chess_server.Models
 {
   [Serializable]
-  public class Player
+  public sealed class Player
   {
     public int Id { get; set; }
     public string Name { get; set; } 
     public string Surname { get; set; }
-    public int LoginId { get; set; }
+
+    public LoginDetails playerLogin { get; set; }
   }
 }
