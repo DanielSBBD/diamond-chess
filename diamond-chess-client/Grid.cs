511c99e4f350370e873f0c08d88b40c21203c2e2
Jesse van der Merwe <jesse@bbd.co.za>
2023-04-19 16:30:53 +0200

    Created the Tile.cs class

--- diamond-chess-client/Grid.cs
+++ diamond-chess-client/Grid.cs
@@ -5,10 +5,8 @@ namespace DiamondChess
 {
 	internal class Grid : Panel
 	{
-		const int gridSize = 8;
 		int radius = 0;
 
-		List<PictureBox> pieces = new List<PictureBox>();
 		List<PictureBox> whiteInventoryPieces = new List<PictureBox>();
 		List<PictureBox> blackInventoryPieces = new List<PictureBox>();
 		int whiteInventoryX = 100;
@@ -16,10 +14,12 @@ namespace DiamondChess
 		int blackInventoryX = 1700;
 		int blackInventoryY = 400;
 
-		PictureBox[,] piecesArray = new PictureBox[gridSize, gridSize];
+		Tile[,] piecesArray = new Tile[Constants.GridSize, Constants.GridSize];
+		public Dictionary<int, (int x, int y)> PositionDictionary = new Dictionary<int, (int, int)>();
 
 		int xCounter = 0;
 		int yCounter = 0;
+		int drawTileCounter = 0;
 
 		protected override void OnPaint(PaintEventArgs e)
 		{
@@ -31,8 +31,10 @@ namespace DiamondChess
 			int heightOffset = 0;
 			xCounter = 0;
 			yCounter = 0;
+			drawTileCounter = 0;
+			PositionDictionary = new Dictionary<int, (int, int)>();
 
-			for(int i = 1; i <= gridSize; i++)
+			for (int i = 1; i <= Constants.GridSize; i++)
 			{
 				widthOffset = Width / 2 - radius * (i-1);
 				heightOffset = radius * i;
@@ -72,25 +74,18 @@ namespace DiamondChess
 					}
 
 					DrawTile(graphics, widthOffset, heightOffset, tempColour, radius);
+					PositionDictionary.Add(drawTileCounter, (widthOffset, heightOffset));
 					widthOffset += radius * 2;
 				}
 			}
-			ConvertPiecesListToArray();
 		}
 
 		private void DrawTile(Graphics graphics, int x, int y, Color color, int radius)
 		{
-			DrawDiamond(graphics, x, y, color, radius);
+			Tile tempPicBox = new Tile(graphics, x, y, color, radius);
+			piecesArray[Constants.CoordinateDictionary[drawTileCounter].x, Constants.CoordinateDictionary[drawTileCounter].y] = tempPicBox;
+			piecesArray[Constants.CoordinateDictionary[drawTileCounter].x, Constants.CoordinateDictionary[drawTileCounter].y].BringToFront();
 
-			PictureBox tempPicBox = new PictureBox();
-			tempPicBox.Size = new Size(radius, radius);
-			tempPicBox.BackgroundImageLayout = ImageLayout.Zoom;
-			tempPicBox.BackColor = Color.Transparent;
-			tempPicBox.BringToFront();
-			tempPicBox.Location = new Point(x - radius / 2, y - radius / 2);
-			tempPicBox.BackgroundImage = null; 
-
-			pieces.Add(tempPicBox);
 			IncrementCounters();
 		}
 
@@ -102,26 +97,22 @@ namespace DiamondChess
 				xCounter = 0;
 				yCounter++;
 			}
+			drawTileCounter++;
 		}
 
 		public void DrawPiece(Image img, int x, int y)
 		{
-			piecesArray[x, y].BackgroundImage = img;
-			piecesArray[x, y].BringToFront();
-			piecesArray[x, y].Refresh();
-		}
+			piecesArray[x, y].SetPiece(img);
+			Controls.Add(piecesArray[x, y]);
 
-		void DrawDiamond(Graphics graphics, int x, int y, Color color, int radius)
-		{
-			Point[] diamondPoints = new Point[4];
+			Graphics graphics = this.CreateGraphics();
 
-			diamondPoints[0] = new Point(x - radius, y);
-			diamondPoints[1] = new Point(x, y - radius);
-			diamondPoints[2] = new Point(x + radius, y);
-			diamondPoints[3] = new Point(x, y + radius);
 
-			SolidBrush brush = new SolidBrush(color);
-			graphics.FillPolygon(brush, diamondPoints);
+			if (x == 7)
+			{
+				piecesArray[x, y].SetOutline(Color.Yellow, graphics);
+				Controls.Add(piecesArray[x, y]);
+			}
 		}
 
 		public void RemovePiece(int x, int y)
@@ -256,203 +247,6 @@ namespace DiamondChess
 			DrawPiece(Properties.Resources.W_Pawn, 2, 3);
 			DrawPiece(Properties.Resources.W_Pawn, 1, 3);
 			DrawPiece(Properties.Resources.W_Pawn, 0, 3);
-		}
-
-		// PLEASE DON'T LOOK HERE THIS IS JUST AWFUL :( 
-		// Yes I'm ashamed, yes there's a MUCH better way to do this, no I don't know what it is, yes it was later 
-		void ConvertPiecesListToArray()
-		{
-			piecesArray[0, 0] = pieces[63];
-			Controls.Add(piecesArray[0, 0]);
-
-			piecesArray[0, 1] = pieces[62];
-			Controls.Add(piecesArray[0, 1]);
-
-			piecesArray[0, 2] = pieces[60];
-			Controls.Add(piecesArray[0, 2]);
-
-			piecesArray[0, 3] = pieces[57];
-			Controls.Add(piecesArray[0, 3]);
-
-			piecesArray[0, 4] = pieces[53];
-			Controls.Add(piecesArray[0, 4]);
-
-			piecesArray[0, 5] = pieces[48];
-			Controls.Add(piecesArray[0, 5]);
-
-			piecesArray[0, 6] = pieces[42];
-			Controls.Add(piecesArray[0, 6]);
-
-			piecesArray[0, 7] = pieces[35];
-			Controls.Add(piecesArray[0, 7]);
-
-			piecesArray[1, 0] = pieces[61];
-			Controls.Add(piecesArray[1, 0]);
-
-			piecesArray[1, 1] = pieces[59];
-			Controls.Add(piecesArray[1, 1]);
-
-			piecesArray[1, 2] = pieces[56];
-			Controls.Add(piecesArray[1, 2]);
-
-			piecesArray[1, 3] = pieces[52];
-			Controls.Add(piecesArray[1, 3]);
-
-			piecesArray[1, 4] = pieces[47];
-			Controls.Add(piecesArray[1, 4]);
-
-			piecesArray[1, 5] = pieces[41];
-			Controls.Add(piecesArray[1, 5]);
-
-			piecesArray[1, 6] = pieces[34];
-			Controls.Add(piecesArray[1, 6]);
-
-			piecesArray[1, 7] = pieces[27];
-			Controls.Add(piecesArray[1, 7]);
-
-			piecesArray[2, 0] = pieces[58];
-			Controls.Add(piecesArray[2, 0]);
-
-			piecesArray[2, 1] = pieces[55];
-			Controls.Add(piecesArray[2, 1]);
-
-			piecesArray[2, 2] = pieces[51];
-			Controls.Add(piecesArray[2, 2]);
-
-			piecesArray[2, 3] = pieces[46];
-			Controls.Add(piecesArray[2, 3]);
-
-			piecesArray[2, 4] = pieces[40];
-			Controls.Add(piecesArray[2, 4]);
-
-			piecesArray[2, 5] = pieces[33];
-			Controls.Add(piecesArray[2, 5]);
-
-			piecesArray[2, 6] = pieces[26];
-			Controls.Add(piecesArray[2, 6]);
-
-			piecesArray[2, 7] = pieces[20];
-			Controls.Add(piecesArray[2, 7]);
-
-			piecesArray[3, 0] = pieces[54];
-			Controls.Add(piecesArray[3, 0]);
-
-			piecesArray[3, 1] = pieces[50];
-			Controls.Add(piecesArray[3, 1]);
-
-			piecesArray[3, 2] = pieces[45];
-			Controls.Add(piecesArray[3, 2]);
-
-			piecesArray[3, 3] = pieces[39];
-			Controls.Add(piecesArray[3, 3]);
-
-			piecesArray[3, 4] = pieces[32];
-			Controls.Add(piecesArray[3, 4]);
-
-			piecesArray[3, 5] = pieces[25];
-			Controls.Add(piecesArray[3, 5]);
-
-			piecesArray[3, 6] = pieces[19];
-			Controls.Add(piecesArray[3, 6]);
-
-			piecesArray[3, 7] = pieces[14];
-			Controls.Add(piecesArray[3, 7]);
-
-			piecesArray[4, 0] = pieces[49];
-			Controls.Add(piecesArray[4, 0]);
-
-			piecesArray[4, 1] = pieces[44];
-			Controls.Add(piecesArray[4, 1]);
-
-			piecesArray[4, 2] = pieces[38];
-			Controls.Add(piecesArray[4, 2]);
-
-			piecesArray[4, 3] = pieces[31];
-			Controls.Add(piecesArray[4, 3]);
-
-			piecesArray[4, 4] = pieces[24];
-			Controls.Add(piecesArray[4, 4]);
-
-			piecesArray[4, 5] = pieces[18];
-			Controls.Add(piecesArray[4, 5]);
-
-			piecesArray[4, 6] = pieces[13];
-			Controls.Add(piecesArray[4, 6]);
-
-			piecesArray[4, 7] = pieces[9];
-			Controls.Add(piecesArray[4, 7]);
-
-			piecesArray[5, 0] = pieces[43];
-			Controls.Add(piecesArray[5, 0]);
-
-			piecesArray[5, 1] = pieces[37];
-			Controls.Add(piecesArray[5, 1]);
-
-			piecesArray[5, 2] = pieces[30];
-			Controls.Add(piecesArray[5, 2]);
-
-			piecesArray[5, 3] = pieces[23];
-			Controls.Add(piecesArray[5, 3]);
-
-			piecesArray[5, 4] = pieces[17];
-			Controls.Add(piecesArray[5, 4]);
-
-			piecesArray[5, 5] = pieces[12];
-			Controls.Add(piecesArray[5, 5]);
-
-			piecesArray[5, 6] = pieces[8];
-			Controls.Add(piecesArray[5, 6]); 
-			
-			piecesArray[5, 7] = pieces[5];
-			Controls.Add(piecesArray[5, 7]);
-
-			piecesArray[6, 0] = pieces[36];
-			Controls.Add(piecesArray[6, 0]);
-
-			piecesArray[6, 1] = pieces[29];
-			Controls.Add(piecesArray[6, 1]);
-		
-			piecesArray[6, 2] = pieces[22];
-			Controls.Add(piecesArray[6, 2]);
-
-			piecesArray[6, 3] = pieces[16];
-			Controls.Add(piecesArray[6, 3]);
-
-			piecesArray[6, 4] = pieces[11];
-			Controls.Add(piecesArray[6, 4]);
-
-			piecesArray[6, 5] = pieces[7];
-			Controls.Add(piecesArray[6, 5]);
-
-			piecesArray[6, 6] = pieces[4];
-			Controls.Add(piecesArray[6, 6]);
-
-			piecesArray[6, 7] = pieces[2];
-			Controls.Add(piecesArray[6, 7]);
-
-			piecesArray[7, 0] = pieces[28];
-			Controls.Add(piecesArray[7, 0]);
-
-			piecesArray[7, 1] = pieces[21];
-			Controls.Add(piecesArray[7, 1]);
-
-			piecesArray[7, 2] = pieces[15];
-			Controls.Add(piecesArray[7, 2]);
-
-			piecesArray[7, 3] = pieces[10];
-			Controls.Add(piecesArray[7, 3]);
-
-			piecesArray[7, 4] = pieces[6];
-			Controls.Add(piecesArray[7, 4]);
-
-			piecesArray[7, 5] = pieces[3];
-			Controls.Add(piecesArray[7, 5]);
-
-			piecesArray[7, 6] = pieces[1];
-			Controls.Add(piecesArray[7, 6]); 
-			
-			piecesArray[7, 7] = pieces[0];
-			Controls.Add(piecesArray[7, 7]);
 		}
 	}
 }
---
+++ diamond-chess-client/Tile.cs
@@ -0,0 +1,60 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace DiamondChess
+{
+	internal class Tile : PictureBox
+	{
+		int xPos, yPos, tileRadius;
+		Color originalTileColor;
+		Point[] diamondPoints = new Point[4];
+
+		public Tile(Graphics graphics, int x, int y, Color color, int radius)
+		{
+			this.xPos = x;
+			this.yPos = y;
+			this.originalTileColor = color;
+			this.tileRadius = radius;
+			this.Size = new Size(radius, radius);
+			this.BackgroundImageLayout = ImageLayout.Zoom;
+			this.BackColor = Color.Transparent;
+			this.Location = new Point(x - radius / 2, y - radius / 2);
+			SetDiamondPoints();
+			FillDiamond(color, graphics);
+		}
+
+		void SetDiamondPoints()
+		{
+			this.diamondPoints[0] = new Point(xPos - tileRadius, yPos);
+			this.diamondPoints[1] = new Point(xPos, yPos - tileRadius);
+			this.diamondPoints[2] = new Point(xPos + tileRadius, yPos);
+			this.diamondPoints[3] = new Point(xPos, yPos + tileRadius);
+		}
+
+		void FillDiamond(Color c, Graphics g)
+		{
+			SolidBrush brush = new SolidBrush(c);
+			g.FillPolygon(brush, diamondPoints);
+		}
+
+		public void SetOutline(Color c, Graphics g)
+		{
+			Pen tempPen = new Pen(c);
+			tempPen.Width = tileRadius / 4;
+			g.DrawPolygon(tempPen, diamondPoints);
+			this.Invalidate();
+		}
+
+		public void SetPiece(Image img)
+		{
+			this.BackgroundImage = img;
+			this.Invalidate();
+		}
+
+		public void HighlightPiece(Color color)
+		{
+			this.BackColor = color;
+			this.Invalidate();
+		}
+	}
+}
