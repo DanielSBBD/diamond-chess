d07748be939e74fa2f554047e335e100ece30518
Gabriel Tidy <gabriel@bbd.co.za>
2023-04-20 14:12:42 +0200

    added match record fields to player

--- diamond-chess-server/Models/Player/Player.cs
+++ diamond-chess-server/Models/Player/Player.cs
@@ -4,9 +4,13 @@
   public sealed class Player
   {
     public int Id { get; set; }
-    public string Name { get; set; } 
-    public string Surname { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
 
-    public LoginDetails playerLogin { get; set; }
+    public int numWins {get; set;}
+    public int numDraws {get; set;}
+    public int numLosses {get; set;}
+
+    public LoginDetails? playerLogin { get; set; }
   }
 }
