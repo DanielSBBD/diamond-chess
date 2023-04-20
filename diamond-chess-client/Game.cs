e24ccc03ab3c0d33fc70d0b9786c0c09227630dc
Gabriel Tidy <gabriel@bbd.co.za>
2023-04-20 14:36:13 +0200

    added win loss record label

--- diamond-chess-client/Game.cs
+++ diamond-chess-client/Game.cs
@@ -1,65 +1,64 @@
-﻿using System;
-using System.Drawing;
-using System.Windows.Forms;
-
-namespace DiamondChess
+﻿namespace DiamondChess
 {
-	public partial class Game : Form
-	{
-		string username1, username2;
-		Grid grid = new Grid();
+  public partial class Game : Form
+  {
+    string username1, username2;
+    Grid grid = new Grid();
 
-		public Game(string u1, string u2)
-		{
-			username1 = u1;
-			username2 = u2;
+    public Game(string u1, string u2)
+    {
+      username1 = u1;
+      username2 = u2;
 
-			InitializeComponent();
-		}
+      InitializeComponent();
+    }
 
-		private void Game_Load(object sender, EventArgs e)
-		{
-			playerTurnText.Text = username1;
+    private void Game_Load(object sender, EventArgs e)
+    {
+      playerTurnText.Text = username1;
 
-			int gridOffset = Height * 10 / 100;
-			int gridSize = Height - gridOffset;
+      winLossRecordLabel.Text = username1 + " W/L/D: " + 0 + "/" + 0 + "/" + 0 + "\n" +
+        username2 + " W/L/D: " + 0 + "/" + 0 + "/" + 0;
 
-			grid.Size = new Size(gridSize, gridSize);
-			grid.Location = new Point(Width / 2 - gridSize / 2, gridOffset / 2);
-			Controls.Add(grid);
-		}
+      int gridOffset = Height * 10 / 100;
+      int gridSize = Height - gridOffset;
 
-		protected override void OnFormClosing(FormClosingEventArgs e)
-		{
-			base.OnFormClosing(e);
-			new Login().Show();
-			Hide();
-		}
+      grid.Size = new Size(gridSize, gridSize);
+      grid.Location = new Point(Width / 2 - gridSize / 2, gridOffset / 2);
+      Controls.Add(grid);
+    }
 
-		private void button1_Click(object sender, EventArgs e)
-		{
-			List<int> xList = new List<int> { 1, 2, 3 };
-			List<int> yList = new List<int> { 1, 2, 4 };
-			List<Color> colorList = new List<Color> { Color.Red, Color.Green, Color.Blue };
+    protected override void OnFormClosing(FormClosingEventArgs e)
+    {
+      base.OnFormClosing(e);
+      new Login().Show();
+      Hide();
+    }
 
-			grid.TakePiece(Properties.Resources.W_Queen, 1, 2, true);
-		}
+    private void button1_Click(object sender, EventArgs e)
+    {
+      List<int> xList = new List<int> { 1, 2, 3 };
+      List<int> yList = new List<int> { 1, 2, 4 };
+      List<Color> colorList = new List<Color> { Color.Red, Color.Green, Color.Blue };
 
-		private void button2_Click(object sender, EventArgs e)
-		{
-			grid.RedrawStartPositions();
-		}
+      grid.TakePiece(Properties.Resources.W_Queen, 1, 2, true);
+    }
 
-		private void button3_Click(object sender, EventArgs e)
-		{
+    private void button2_Click(object sender, EventArgs e)
+    {
+      grid.RedrawStartPositions();
+    }
 
-			List<int> xList = new List<int> { 1, 2, 3 };
-			List<int> yList = new List<int> { 1, 2, 4 };
-			List<Color> colorList = new List<Color> { Color.Red, Color.Green, Color.Blue };
-			grid.HighlightPieces(xList, yList, colorList);
-		}
+    private void button3_Click(object sender, EventArgs e)
+    {
+
+      List<int> xList = new List<int> { 1, 2, 3 };
+      List<int> yList = new List<int> { 1, 2, 4 };
+      List<Color> colorList = new List<Color> { Color.Red, Color.Green, Color.Blue };
+      grid.HighlightPieces(xList, yList, colorList);
+    }
 
 
 
-	}
+  }
 }
